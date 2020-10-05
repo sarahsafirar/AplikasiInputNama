d8a24c9d1b2fdb422b0f6537386a660b85da82ca
user1 <sarahsfir84@gmail.com>
2020-10-05 18:09:37 +0700

    commit2

---
+++ app/src/main/java/org/example/aplikasiinputnama/MainActivity.java
@@ -0,0 +1,26 @@
+package org.example.aplikasiinputnama;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText namaEditText;
+    private TextView namaTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        namaEditText = findViewById(R.id.namaEditText);
+        namaTextView = findViewById(R.id.namaTextView);
+    }
+
+    public void tampilkan(View view) {
+        namaTextView.setText("Nama anda: "+namaEditText.getText());
+    }
+}
